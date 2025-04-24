b7c6a78f1d074da138397e5a75c9ac5f0757ecb4
Daniel <daniel.vartanov81@gmail.com>
2025-04-24 20:18:06 +0400

    fix

--- src/app/pages/movies/Layout.tsx
+++ src/app/pages/movies/Layout.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-// import SpiderMan from "./SpiderMan";
-// import Interstellar from "./Interstellar";
-// import GreenBook from "./GreenBook";
+import type { LayoutProps } from "@/types";
 
-type LayoutProps = {
-  children: React.ReactNode;
-}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* <Navbar />
-      <main className="flex-grow container mx-auto px-4 py-6">*/}
         {children}
-      {/*</main>
-      <Interstellar /> */}
     </div>
   );
 };
