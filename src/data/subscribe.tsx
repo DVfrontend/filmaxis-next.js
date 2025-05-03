f32ed748b5e00ff105441287564abb806f2c14e6
Daniel <daniel.vartanov81@gmail.com>
2025-05-03 21:31:50 +0400

    fix

--- src/data/subscribe.tsx
+++ src/data/subscribe.tsx
@@ -3,32 +3,42 @@
 import data from "./subscribe.json";
 import { useEffect, useState } from "react";
 import type { Subscribes } from "@/types";
-import Link from "next/link";
 
 export default function SubscribePage() {
-  const [subscribe, setSubscribe] = useState<Subscribes[]>([]);
+  const [subscribes, setSubscribes] = useState<Subscribes[]>([]);
 
   useEffect(() => {
-    setSubscribe(data);
+    setSubscribes(data);
   }, []);
 
   return (
-    <>
-      <h1 className="text-4xl font-semibold mb-6 text-center">Оформить подписку на сайт</h1>
-      {subscribe.map((sub) => (
-        <Link href={sub.billingCycle} key={sub.id}>
-          <div className="border rounded-sm p-4 text-center break-words">
-            <div className="m-2 p-2">
-              <span className="font-bold flex justify-center m-2 text-xl">
-                {sub.name}
-              </span>
-              <p>{sub.description}</p>
-              <ul>
-                <li>{sub.features}</li></ul>
+    <main className="max-w-5xl mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Оформить подписку
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {subscribes.map((sub) => (
+          <div
+            key={sub.id}
+            className="border rounded-md p-4 flex flex-col justify-between hover:shadow-md transition"
+          >
+            <div>
+              <h2 className="text-xl font-semibold mb-2 text-center">{sub.name}</h2>
+              <p className=" text-gray-300 text-center mb-4">{sub.description}</p>
+              <ul className="list-disc list-inside space-y-1">
+                {sub.features.map((feature, index) => (
+                  <li key={index}>{feature}</li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="mt-4 text-center font-bold text-lg">
+              {sub.price} {sub.currency}
             </div>
           </div>
-        </Link>
-      ))}
-    </>
+        ))}
+      </div>
+    </main>
   );
 }
