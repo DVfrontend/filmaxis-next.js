66062e55526972283a56a054f538d19fd8549b45
Daniel <daniel.vartanov81@gmail.com>
2025-05-06 21:32:55 +0400

    last fix

--- src/app/movies/matrix/page.tsx
+++ src/app/movies/matrix/page.tsx
@@ -4,7 +4,7 @@ import type { Film } from "@/types";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
-export default function Avatar() {
+export default function Matrix() {
   const [films, setFilms] = useState<Film[]>([]);
   const targetId = 6;
 
@@ -17,26 +17,32 @@ export default function Avatar() {
   return (
     <>
       {filteredFilms.map((film) => (
-        <div key={film.id}>
-          <div className="flex justify-evenly">
+        <div key={film.id} className="p-6">
+          <div className="flex flex-col md:flex-row items-start gap-8 mb-8">
             <Image
               src={film.poster}
               alt={film.name}
-              width={200}
+              width={250}
               height={200}
               className="border rounded-xl"
             />
-            <h1 className="text-2xl">{film.name}</h1>
-            <p>{film.fulldescription}</p>
+
+            <div className="flex-1">
+              <h1 className="text-4xl font-bold mb-4">{film.name}</h1>
+              <p className="text-lg">{film.fulldescription}</p>
+            </div>
           </div>
-          <h3 className="text-xl m-2">Трейлер фильма</h3>
-          <iframe
-            src={film.trailer}
-            width="560"
-            height="315"
-            className="aspect-video rounded-xl"
-            allowFullScreen
-          />
+
+          <div className="mt-8">
+            <h3 className="text-2xl font-semibold mb-4">Трейлер фильма</h3>
+            <iframe
+              src={film.trailer}
+              width="100%"
+              height="300"
+              className="rounded-xl sm:w-24"
+              allowFullScreen
+            />
+          </div>
         </div>
       ))}
     </>
